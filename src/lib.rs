e5dd1f0856857d6c8a9ca2424ea31e9cc72003d7
Mitsuharu Seki <mitsu1986@gmail.com>
2017-10-27 07:56:45 +0900

    Add Readme, Cargo.toml, .travis.tml

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![crate_type = "lib"]
+
+//! rust-compression
+//!
+//! # Licensing
+//! This Source Code is subject to the terms of the Mozilla Public License
+//! version 2.0 (the "License"). You can obtain a copy of the License at
+//! http://mozilla.org/MPL/2.0/ .
+
